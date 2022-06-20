170a5078b94b35e8da27df90943ebe307f9372fd
daoif <1143201754@qq.com>
2022-06-21 07:29:18 +0800

    new 1

--- lib/enc/enctest.ts
+++ lib/enc/enctest.ts
@@ -2,7 +2,9 @@ import { iocSocket } from "erpc";
 
 interface IEnctest {
   (_cioSocket: iocSocket): void;
-  sendtotest1(arg: any): Promise<number | any>;
+  /**基础发送封装,仅限enc内部使用 */
+  base(arg: any, apiname: string): Promise<number | any>;
+  test1(arg: any): Promise<number | any>;
 }
 //这个cio不要放到属性里,否则初始化麻烦.
 let cioSocket: iocSocket;
@@ -12,24 +14,33 @@ export const enctest: IEnctest = (_cioSocket: iocSocket) => {
 };
 
 //消费api,传入参数,等待回调.一般来说是async,里面封装成await promise.
-enctest.sendtotest1 = async (arg: any) => {
+enctest.base = async (arg: any, apiname: string) => {
   if (cioSocket?.connected == undefined) {
-    console.log("生产端不在线, sendtotest1 发送失败");
+    console.log("生产端不在线, " + apiname + " 发送失败");
     return -1;
   }
   //此处2个any,都应该更换为具体的类型
   let res2: any = await new Promise((res) => {
-    cioSocket.timeout(1000).emit("test1", arg, (err: any, response: any) => {
+    cioSocket.timeout(1000).emit(apiname, arg, (err: any, response: any) => {
       if (err) {
-        console.log("请求 test1,回调超时报错:");
+        console.log("请求 " + apiname + ",回调超时报错:");
         console.log(err);
         res(err);
       } else {
-        console.log("请求 test1,回调数据:");
+        console.log("请求 " + apiname + ",回调数据:");
         console.log(response);
         res(response);
       }
     });
   });
+  return res2;
+};
+
+//消费api,传入参数,等待回调.一般来说是async,里面封装成await promise.
+enctest.test1 = async (arg: any) => {
+  let res2: any = await new Promise((res) => {
+    res(enctest.base(arg, "test1"));
+  });
+
   return res2;
 };
